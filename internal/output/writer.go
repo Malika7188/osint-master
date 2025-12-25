9781766f244e44de206fc375e750bbc9c5f9ff77
Malika <asmanodeny@gmail.com>
2025-12-25 19:50:56 +0300

    Add SaveToFile helper to persist output reports with timestamp

--- internal/output/writer.go
+++ internal/output/writer.go
@@ -8,4 +8,8 @@ import (
 
 // SaveToFile saves the result string to a file
 func SaveToFile(filename string, content string) error {
+	if filename == "" {
+		return fmt.Errorf("filename cannot be empty")
+	}
+
 	
