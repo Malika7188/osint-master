e488ba2b18945353b834c1c540786f72bdd48b2d
Malika <asmanodeny@gmail.com>
2025-12-26 22:29:53 +0300

    feat: implement advanced domain lookup with WHOIS integration

--- internal/scraper/truecaller_scraper.js
+++ internal/scraper/truecaller_scraper.js
@@ -2,3 +2,10 @@
 
 const { chromium } = require('playwright');
 
+async function scrapeTrueCaller(phoneNumber) {
+    const browser = await chromium.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    
